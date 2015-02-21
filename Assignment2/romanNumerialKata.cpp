8c0123c949a580f8acefb49e73a9fe0b097cef01
Dylan <dylan_w_tucker@hotmail.com>
2015-02-20 22:39:12 -0900

    Passes 4 Tests - added if statement to handle 10 Added Test 5(30 == XXX) Fails

--- Assignment2/romanNumerialKata.cpp
+++ Assignment2/romanNumerialKata.cpp
@@ -8,8 +8,10 @@
 std::string toRoman(int arabic)
 {
     std::string roman = "";
+    if(arabic == 10)
+        return "X";
     while(arabic-- > 0)
-    roman += "I";
+        roman += "I";
 
     return roman;
 }
@@ -23,6 +25,7 @@ TEST_CASE("Arabic To Roman Conversion", "[toRoman]")
     REQUIRE( toRoman(2) == "II");
     REQUIRE( toRoman(3) == "III");
     REQUIRE( toRoman(10) == "X");
+    REQUIRE( toRoman(30) == "XXX");
 }
 
 
