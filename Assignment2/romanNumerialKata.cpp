c33e821ec814c106a1ecfd4a7fbe582ff12dae40
Dylan <dylan_w_tucker@hotmail.com>
2015-02-20 22:27:35 -0900

    Passes First Test Added Second Test Fails

--- Assignment2/romanNumerialKata.cpp
+++ Assignment2/romanNumerialKata.cpp
@@ -1,5 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <string>
+
+
+
+
+std::string toRoman(int arabic)
+{
+    return "I";
+}
+
+
+
+
 TEST_CASE("Arabic To Roman Conversion", "[toRoman]")
 {
     REQUIRE( toRoman(1) == "I");
