d6dcb5320f98ac772d0106b3e7ad1e7b9519eacc
Dylan <dylan_w_tucker@hotmail.com>
2015-02-20 22:34:16 -0900

    Passes 3 Tests Added While Loop Added 4th Test

--- Assignment2/romanNumerialKata.cpp
+++ Assignment2/romanNumerialKata.cpp
@@ -7,9 +7,11 @@
 
 std::string toRoman(int arabic)
 {
-    if(arabic == 2)
-    return "II";
-    return "I";
+    std::string roman = "";
+    while(arabic-- > 0)
+    roman += "I";
+
+    return roman;
 }
 
 
@@ -20,6 +22,7 @@ TEST_CASE("Arabic To Roman Conversion", "[toRoman]")
     REQUIRE( toRoman(1) == "I");
     REQUIRE( toRoman(2) == "II");
     REQUIRE( toRoman(3) == "III");
+    REQUIRE( toRoman(10) == "X");
 }
 
 
